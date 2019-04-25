7049dd2d3398da71998ff3ebdab657f6991c192c
Alan <mashalonzo741@gmail.com>
2019-04-25 16:32:37 +0300

    add changes to the script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
-function add(number1, number2) {
-return number1 + number2;
-};
-alert(add(10,5));
-
 var add=function(number1, number2){
 return number1 + number2;
 };
 var number1=parseInt(prompt("Enter a number:"));
 var number2=parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+var result=add(number1, number2);
+alert(result);
+var subtract=function(number1, number2){
+return number1-number2;
+}
+var number1=parseInt(prompt("Enter a number:"));
+var number2=parseInt(prompt("Enter another number:"));
+var result=subtract(number1,number2);
+alert(result);
+var multiply=function multiply(number1,number2){
+return number1*number2;
+}
+var number1=parseInt(prompt("Enter a number:"));
+var number2=parseInt(prompt("Enter another number:"));
+var multiply=multiply(number1, number2);
+alert(result);
 
+var divide=function divide(number1,number2){
+return number1/number2;
+}
+var number1=parseInt(prompt("Enter a number:"));
+var number2=parseInt(prompt("Enter another number:"));
+var divide=divide(number1, number2);
+alert(result);
